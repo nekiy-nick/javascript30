c5bab2c223d3e13f4ffdf0d98ca86cc647d72c6f
Nickolai Khitrov <deliriumbred@yandex.ru>
2022-02-12 11:55:07 +0300

    Make challenge with ajax type ahead with fetch

---
+++ 6 Ajax Type Ahead with fetch/script.js
@@ -0,0 +1,39 @@
+const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+const cities = [];
+
+fetch(endpoint)
+  .then(blob => blob.json())
+  .then(data => cities.push(...data));
+
+function findMatches(wordToMatch, cities) {
+  return cities.filter(place => {
+    const regex = new RegExp(`${wordToMatch}`, 'gi');
+    return place.city.match(regex) || place.state.match(regex);
+  });
+}
+
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
+function displayMatches() {
+  const matchArray = findMatches(this.value, cities);
+  const html = matchArray.map(place => {
+    const regex = new RegExp(this.value, 'g');
+    const cityName = place.city.replace(regex, `<mark class="marked">${this.value}</mark>`);
+    const stateName = place.state.replace(regex, `<mark class="marked marked_state">${this.value}</mark>`);
+    return `
+      <li>
+        <span class="name">${cityName}, ${stateName}</span>
+        <span class="population">Pop: ${numberWithCommas(place.population)}</span>
+      </li>
+    `
+  }).join('');
+  suggestions.innerHTML = html;
+}
+
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
