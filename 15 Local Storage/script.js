8160d4e6a52082fbb4399dcb3b197d7e883effc0
Nickolai Khitrov <deliriumbred@yandex.ru>
2022-02-24 18:30:53 +0300

    Training with local storage api

--- 15 Local Storage/script.js
+++ 15 Local Storage/script.js
@@ -1,3 +1,54 @@
 const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
-const items = [];
+let items = JSON.parse(localStorage.getItem('items')) || [];
+const clearItemsBtn = addItems.querySelector('.clear-items-btn');
+
+function addItem(e) {
+    e.preventDefault();
+    let text = this.querySelector('input[name=item]').value;
+    const item = {
+      text,
+      done: false
+    };
+
+    items.push(item);
+    populateList(items, itemsList);
+    localStorage.setItem('items', JSON.stringify(items));
+    this.reset();
+}
+
+function populateList(plates = [], platesList) {
+  platesList.innerHTML = plates.map((plate, i) => {
+    return `
+      <li>
+        <input type="checkbox" data-index=${i} ${plate.done ? 'checked' : ''} id=${i}>
+        <label for=${i}>${plate.text}</label>
+      </li>
+    `
+  }).join('');
+
+  plates.length > 0 ? clearItemsBtn.removeAttribute('disabled') : clearItemsBtn.setAttribute('disabled', '');
+}
+
+function toggleDone(e) {
+  const el = e.target;
+  if (!el.matches('input')) return;  
+  const index = el.dataset.index;
+  items[index].done = !items[index].done;
+  localStorage.setItem('items', JSON.stringify(items));
+  populateList(items, itemsList);
+}
+
+function clearPlates(e) {
+  items = [];
+  populateList([], itemsList);
+  localStorage.removeItem('items');
+  itemsList.innerHTML = `
+    <li>Loading Tapas...</li>
+  `;
+}
+
+addItems.addEventListener('submit', addItem);
+itemsList.addEventListener('click', toggleDone);
+populateList(items, itemsList);
+clearItemsBtn.addEventListener('click', clearPlates);
