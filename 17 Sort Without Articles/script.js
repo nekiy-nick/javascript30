1f098c11b0d01eb3d7238122c256f448341751a7
Nickolai Khitrov <deliriumbred@yandex.ru>
2022-03-02 14:17:04 +0300

    Make functional sort array of strings without articles

--- 17 Sort Without Articles/script.js
+++ 17 Sort Without Articles/script.js
@@ -1,3 +1,19 @@
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
 
 const bandsList = document.querySelector('#bands');
+
+function checkStringOnArticle(string) {
+  return string.replace(/^(a |the |an )/i, '').trim();
+}
+
+function sortWithoutArticles() {
+  return [...bands].sort((prevString, nextString) => checkStringOnArticle(prevString) > checkStringOnArticle(nextString) ? 1 : -1);
+}
+
+const resultArray = sortWithoutArticles();
+
+bandsList.innerHTML = resultArray.map(item => {
+  return `
+    <li>${item}</li>
+  `
+}).join('');
