8a2503b0467a1843fb8949319229587da02a5e1b
Nickolai Khitrov <deliriumbred@yandex.ru>
2022-03-03 17:04:22 +0300

    Training with geolocation api. Doesnt word anywhere

--- 21 Geolocation/script.js
+++ 21 Geolocation/script.js
@@ -1,1 +1,11 @@
 /*Compass: https://thenounproject.com/search/?q=compass&i=592352*/
+const arrow = document.querySelector('.arrow');
+const speed = document.querySelector('.speed-value');
+
+navigator.geolocation.watchPosition((data) => {
+  console.log(data)
+  speed.innerText = data.coords.speed || 0;
+  arrow.style.transform = `rotate(${data.coords.heading || 0}deg)`;
+}, err => {
+  alert('HEY! YOU GOTTA ALLOW THAT TO HAPPEN!!!');
+})
