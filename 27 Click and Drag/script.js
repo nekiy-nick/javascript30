9b60fd3113e4564069b3d20013d85a5450aad81c
Nickolai Khitrov <deliriumbred@yandex.ru>
2022-03-05 15:49:02 +0300

    Finish training with click and drop challenge

--- 27 Click and Drag/script.js
+++ 27 Click and Drag/script.js
@@ -1,1 +1,30 @@
+const slider = document.querySelector('.items');
+let isDown = false;
+let startX;
+let scrollLeft;
 
+function isDownOn(e) {
+  isDown = true;
+  slider.classList.add('active');
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+}
+
+function drag(e) {
+  if (!isDown) return;
+
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 2;
+  slider.scrollLeft = scrollLeft - walk;
+}
+
+function stopDrag() {
+  isDown = false;
+  slider.classList.remove('active');
+}
+
+slider.addEventListener('mousedown', isDownOn);
+slider.addEventListener('mouseleave', stopDrag);
+slider.addEventListener('mouseup', stopDrag);
+slider.addEventListener('mousemove', drag);
